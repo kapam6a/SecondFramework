7833cc1a073a647358c6d8480534e1059ea5b90b
Aleksey Yakimenko <a.yakimenko@rambler-co.ru>
2018-03-29 12:50:14 +0300

    Добавил хедер.

--- SecondFramework/SecondFramework.h
+++ SecondFramework/SecondFramework.h
@@ -14,6 +14,6 @@ FOUNDATION_EXPORT double SecondFrameworkVersionNumber;
 //! Project version string for SecondFramework.
 FOUNDATION_EXPORT const unsigned char SecondFrameworkVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <SecondFramework/PublicHeader.h>
+ #import "PublicHeader.h"
 
 
